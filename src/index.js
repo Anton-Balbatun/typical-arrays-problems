6e646185b554c4bf631bdd8be21945234796d7c6
Anton-Balbatun <anton553322@gmail.com>
2020-03-18 21:32:45 +0300

    add typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array==''){return 0}else{
+  array.sort(function(a, b) {
+    return a - b;
+  })
+  return array[0];
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array==''){return 0}else{
+  array.sort(function(a, b) {
+    return a - b;
+  })
+  return array[array.length-1];
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum =0
+  if(!array || array==''){return 0}else{
+   
+    array.map((element)=>{
+    sum = sum+element
+  })
+  return sum/array.length
+  }
 }
